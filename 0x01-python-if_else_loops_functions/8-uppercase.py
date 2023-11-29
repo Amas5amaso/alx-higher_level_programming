ce5b4f54085ac989cd172fb91824daf49a7b970b
Amas5amaso <ansamatemeso@gmail.com>
2023-11-29 12:50:53 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
-uppercase = __import__('8-uppercase').uppercase
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    for cin str:
+        print("(:c)"
+                .format(ord(c) if not islower(c) else ord - 32),
+                print("")
