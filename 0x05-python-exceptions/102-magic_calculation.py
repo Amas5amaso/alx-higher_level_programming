5bc8d5974728c83a55aa86f8b8c1258cc2286de0
Amas5amaso <ansamatemeso@gmail.com>
2023-12-18 13:54:18 -0800

    to update

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    """This function performs a calculation given by args"""
+    a: The first operand
+    b: The second operand
+    return:
+    """A value based on the specified operation"""
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise Exception("Too far")
             result += pow(a, b) / i
         except Exception:
             result = b + a
