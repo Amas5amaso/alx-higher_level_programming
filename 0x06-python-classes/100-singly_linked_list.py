6bb5689205a08af88a43e02f5a28032b6a0d3409
Amas5amaso <ansamatemeso@gmail.com>
2023-12-19 10:53:25 -0800

    commited

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -18,28 +18,31 @@ class SinglyLinkedList:
         self.head = None
 
     def __repr__(self):
-        if self.head is None:
-            return "Empty list"
-        result = []
+        return " -> ".join(str(node.data) for node in self)
+
+    def __iter__(self):
         node = self.head
         while node is not None:
-            result.append(str(node.data))
+            yield node
             node = node.next_node
-        return "\n".join(result)
 
     def sorted_insert(self, value):
-        if not isinstance(value, int):
-            raise TypeError("value must be an integer")
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
-            return
-        if self.head.data > value:
+        elif self.head.data > value:
             new_node.next_node = self.head
             self.head = new_node
-            return
-        node = self.head
-        while node.next_node is not None and node.next_node.data < value:
-            node = node.next_node
-        new_node.next_node = node.next_node
-        node.next_node = new_node
+        else:
+            current_node = self.head
+            while current_node.next_node is not None and current_node.next_node.data < value:
+                current_node = current_node.next_node
+            new_node.next_node = current_node.next_node
+            current_node.next_node = new_node
+
+            list = SinglyLinkedList()
+list.sorted_insert(5)
+list.sorted_insert(3)
+list.sorted_insert(1)
+list.sorted_insert(7)
+print(list) # prints: Node(1) -> Node(3) -> Node(5) -> Node(7)
