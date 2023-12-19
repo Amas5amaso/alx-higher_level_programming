bfa0c72fb088f87063f11931a3709675cb56f4e8
Amas5amaso <ansamatemeso@gmail.com>
2023-12-19 11:12:29 -0800

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 """square module."""
+
+
 class Square:
-    """Defines a square."""
     pass
-list = SinglyLinkedList()
-list.sorted_insert(Square())
-print(list) # prints: Node(<__main__.Square object at 0x7f021b3f9940>)
